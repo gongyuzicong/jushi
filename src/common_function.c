1af55c6564350c84d20b9086f39d09bfb2117e26
Zion.Guo <gongyuzicong@163.com>
2016-08-16 16:16:26 +0800

    recovery file common_include.h common_function.c v4.8

--- src/common_function.c
+++ src/common_function.c
@@ -1,10 +1,11 @@
 #include "common_include.h"
+#include "buffer.h"
 
 vu32 SystemRunningTime = 0x00000000;
 
 /*******************************************************************************
 * 函数名  		: fputc
-* 函数描述    	: 将printf函数重定位到USATR
+* 函数描述    	: 将printf函数重定位到USATR1
 * 输入参数    	: 无
 * 输出结果    	: 无
 * 返回值		: 无
